97179f632faeeb78389752de79c2d0358d1c1522
farrelathalla <farrelxag@gmail.com>
2024-11-23 12:44:32 +0700

    feat: add hero

---
+++ src/frontend/components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='p-4 box-border'>
+        <div className='flex w-full min-h-screen gap-4'>
+            {/* Left */}
+            <div className='w-[30%] bg-[#121212] rounded-lg'>
+                tes
+            </div>
+            {/* Right */}
+            <div className='w-[70%] bg-[#202020] rounded-lg'>
+                tes
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
